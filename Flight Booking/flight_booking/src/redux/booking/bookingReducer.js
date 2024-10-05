3685f99d1cc6a24dc99c5c33cfc81ce918a5ee9b
rajon-git <rajon.zhsust15@gmail.com>
2024-10-05 21:40:06 +0600

    add booking reducer create

--- Flight Booking/flight_booking/src/redux/booking/bookingReducer.js
+++ Flight Booking/flight_booking/src/redux/booking/bookingReducer.js
@@ -1,2 +1,18 @@
-export const BOOKING = "booking/booking";
-export const DELETE = "booking/delete";
+import { BOOKING } from "./actionTypes";
+
+const initialState = [];
+
+const bookingReducer = (state=initialState,action) => {
+    const copiedState = [...state]
+    const {type,payload,id} = action;
+
+    switch(type) {
+        case BOOKING:
+            const lastBookid = copiedState[copiedState.length -1]?.id;
+            const newBook = {...payload, id: lastBookid ? (lastBookid + 1) : 1};
+            copiedState.push(newBook)
+            return copiedState;
+        default:
+            return state;
+    }
+}
