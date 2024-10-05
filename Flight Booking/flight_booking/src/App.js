899d5abfd380195ccea8925b75dda4f334171b5a
rajon-git <rajon.zhsust15@gmail.com>
2024-10-05 21:46:34 +0600

    app files updated there redux store

--- Flight Booking/flight_booking/src/App.js
+++ Flight Booking/flight_booking/src/App.js
@@ -2,14 +2,16 @@ import './styles.css';
 import Header from './components/Header/Header';
 import InputSection from './components/Section/InputSection';
 import BookList from './components/Section/BookList';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <InputSection />
       <BookList />
-    </>
+    </Provider>
   );
 }
 
--- Flight Booking/flight_booking/src/redux/store.js
+++ Flight Booking/flight_booking/src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore } from "react-redux";
+import { createStore } from "redux";
 import bookingReducer from "./booking/bookingReducer";
 
 const store = createStore(bookingReducer);
