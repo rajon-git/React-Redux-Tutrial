44140336ea5be8a28b724139916411c767bece14
rajon-git <rajon.zhsust15@gmail.com>
2024-10-05 21:43:09 +0600

    delete reducer created

--- Flight Booking/flight_booking/src/redux/booking/bookingReducer.js
+++ Flight Booking/flight_booking/src/redux/booking/bookingReducer.js
@@ -1,4 +1,4 @@
-import { BOOKING } from "./actionTypes";
+import { BOOKING, DELETE } from "./actionTypes";
 
 const initialState = [];
 
@@ -12,7 +12,12 @@ const bookingReducer = (state=initialState,action) => {
             const newBook = {...payload, id: lastBookid ? (lastBookid + 1) : 1};
             copiedState.push(newBook)
             return copiedState;
+        case DELETE:
+            const updateState = copiedState.filter(item => item.id !== id)
+            return updateState;
         default:
             return state;
     }
 }
+
+export default bookingReducer;
