dbedf359542cdc2df61cc1d93308bb93a19066df
rajon-git <rajon.zhsust15@gmail.com>
2024-10-06 20:46:24 +0600

    todo application react app setup

---
+++ Module-3/src/App.js
@@ -0,0 +1,21 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div class="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+      <Navbar />
+      <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+        <Header />
+        <hr class="mt-4" />
+        <TodoList />
+        <hr class="mt-4" />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
