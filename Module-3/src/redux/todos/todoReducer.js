a4f1a5fcda90b0ff2cfb2341a01c33ea9e1810c9
rajon-git <rajon.zhsust15@gmail.com>
2024-10-07 23:32:51 +0600

    todo added created"

--- Module-3/src/redux/todos/todoReducer.js
+++ Module-3/src/redux/todos/todoReducer.js
@@ -1,3 +1,21 @@
-const todoReducer = (state= initialState,action) => {
+import { ADDED } from "./actionTypes";
+import { initialState } from "./initialState";
 
+function nextTodoId = (todos) => {
+    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+    return maxId + 1;
+}
+const todoReducer = (state= initialState ,action) => {
+    switch (action.type) {
+        case ADDED:
+            return [
+                ...state,
+                {
+                    id: nextTodoId(state)
+                }
+            ]
+    
+        default:
+            break;
+    }
 }
