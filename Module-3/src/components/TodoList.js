46de1c296abf8a37a9442eda50eaa73c6224af6f
rajon-git <rajon.zhsust15@gmail.com>
2024-10-13 15:04:22 +0600

    filter section dynamically updated

--- Module-3/src/components/TodoList.js
+++ Module-3/src/components/TodoList.js
@@ -4,10 +4,35 @@ import { useSelector } from "react-redux";
 
 export default function TodoList() {
   const todos = useSelector((state)=>state.todos)
+  const filters = useSelector((state)=>state.filters)
+
+  const filterByStatus = (todo) => {
+    const {status} = filters;
+    switch (status) {
+      case 'Complete':
+        return todo.completed;
+      case 'Incomplete':
+        return !todo.completed
+      default:
+        return true;
+    }
+  }
+
+  const filterByColor = (todo) => {
+    const {colors} = filters;
+    if(colors.length > 0 ) {
+      return colors.includes(todo?.color)
+    }
+    return true
+  }
+
   return (
     <div>
       <div class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-        {todos.map((todo) => (<Todo todo={todo} key={todo.id}/>))}
+        {todos
+        .filter(filterByStatus)
+        .filter(filterByColor)
+        .map((todo) => (<Todo todo={todo} key={todo.id}/>))}
       </div>
     </div>
   );
